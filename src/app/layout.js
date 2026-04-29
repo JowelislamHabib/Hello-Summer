f4529ef180aaaa81e06d5b7ae75371344ecd7dc4
JowelislamHabib <jowel@bintofajjal.com>
2026-04-29 21:03:29 +0600

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Hello Summer",
+  description: "Premium Men’s Seasonal Essentials",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${dmSans.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div>This Is a navbar</div>;
+};
+
+export default NavBar;
