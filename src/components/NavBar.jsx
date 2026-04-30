9435630cb6a00d2b3b0608f35bfb804365f96c90
JowelislamHabib <jowel@bintofajjal.com>
2026-04-30 12:53:18 +0600

    feat: Add solis logo images and update NavBar component styles for improved layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   return (
     <div className="bg-orange-50 shadow-sm sticky top-0 z-50">
       <div className="container mx-auto">
-        <div className="  flex items-center justify-between px-8 h-20">
+        <div className="  flex items-center justify-between px-4 md:px-0 h-20">
           <div className="flex items-center gap-2">
             <IoSunnyOutline className="text-3xl text-orange-400 " />
             <span className="text-2xl font-black text-gray-900 font-serif ">
@@ -40,14 +40,14 @@ const NavBar = () => {
 
             <Link
               href={"/login"}
-              className="flex justify-center items-center gap-2 px-8 py-2.5 rounded-xl border-2 border-amber-500 text-stone-900 font-bold hover:shadow-[0_8px_24px_rgba(245,158,11,0.4)] transition-all active:scale-95 hidden md:flex"
+              className="flex justify-center items-center gap-2 px-8 py-2.5 rounded-full border-2 border-amber-500 text-stone-900 font-bold hover:shadow-[0_8px_24px_rgba(245,158,11,0.4)] transition-all active:scale-95 hidden md:flex"
             >
               <IoLogIn size={24} />
               Login
             </Link>
             <Link
               href={"/register"}
-              className="flex justify-center items-center gap-2 px-8 py-2.5 rounded-xl border-2 border-amber-500 bg-linear-to-r from-amber-500 to-orange-500 text-stone-900 font-bold hover:shadow-[0_8px_24px_rgba(245,158,11,0.4)] transition-all active:scale-95 hidden md:flex"
+              className="flex justify-center items-center gap-2 px-8 py-2.5 rounded-full border-2 border-amber-500 bg-linear-to-r from-amber-500 to-orange-500 text-stone-900 font-bold hover:shadow-[0_8px_24px_rgba(245,158,11,0.4)] transition-all active:scale-95 hidden md:flex"
             >
               <IoPersonAdd />
               Register
